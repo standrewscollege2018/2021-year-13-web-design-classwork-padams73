7d1cb7891327922d9af15b0db0b48836a63e44f2
padams73 <pad@stac.school.nz>
2021-02-15 10:43:38 +1300

    Studentdb

---
+++ Studentdb/addtutor.php
@@ -0,0 +1,13 @@
+<h1>Add new tutor</h1>
+<form action="index.php?page=entertutor" method="post">
+  <div class="mb-3">
+    <label for="tutorname" class="form-label">Tutor name</label>
+    <input name="tutor" type="text" class="form-control" id="tutorname" aria-describedby="tutorname">
+  </div>
+  <div class="mb-3">
+    <label for="tutorcode" class="form-label">Tutor code</label>
+    <input name="tutorcode" type="text" class="form-control" id="tutorcode">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
