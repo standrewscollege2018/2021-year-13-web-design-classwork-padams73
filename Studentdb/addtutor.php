0e2f43040bf23227abd971f58d5766de47060fdb
padams73 <pad@stac.school.nz>
2021-03-11 08:48:25 +1300

    Adding tutor group

--- Studentdb/addtutor.php
+++ Studentdb/addtutor.php
@@ -1,13 +1,15 @@
 <h1>Add new tutor</h1>
-<form action="index.php?page=entertutor" method="post">
+<form action="index.php?page=entertutor" method="post" enctype="multipart/form-data">
   <div class="mb-3">
     <label for="tutorname" class="form-label">Tutor name</label>
-    <input name="tutor" type="text" class="form-control" id="tutorname" aria-describedby="tutorname">
+    <input name="name" type="text" class="form-control" id="tutorname" aria-describedby="tutorname">
   </div>
   <div class="mb-3">
     <label for="tutorcode" class="form-label">Tutor code</label>
     <input name="tutorcode" type="text" class="form-control" id="tutorcode">
   </div>
-
+  <div class="mb-3">
+    <input type="file" name="fileToUpload" id="fileToUpload">
+  </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
---
+++ Studentdb/entertutor.php
@@ -0,0 +1,82 @@
+<?php
+// Check for duplicates first
+$name = $_POST['name'];
+$tutorcode = $_POST['tutorcode'];
+
+$checktutor_sql = "SELECT * FROM tutorgroup WHERE tutor='$name' or tutorcode='$tutorcode'";
+
+$checktutor_qry = mysqli_query($dbconnect, $checktutor_sql);
+
+if(mysqli_num_rows($checktutor_qry)==0) {
+
+
+  $target_dir = "images/";
+  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $uploadOk = 1;
+  $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+  // Check if image file is a actual image or fake image
+  if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+      echo "File is an image - " . $check["mime"] . ".";
+      $uploadOk = 1;
+    } else {
+      echo "File is not an image.";
+      $uploadOk = 0;
+    }
+  }
+
+  // Check if file already exists
+  if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+  }
+
+  // Check file size
+  if ($_FILES["fileToUpload"]["size"] > 5000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+  }
+
+  // Allow certain file formats
+  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+  && $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+  }
+
+  // Check if $uploadOk is set to 0 by an error
+  if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+  // if everything is ok, try to upload file
+  } else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      // Get the name of the image being uploaded
+      $photo = $_FILES["fileToUpload"]["name"];
+      // Insert new record into the database
+      $addtutor_sql = "INSERT into tutorgroup (tutor, tutorcode, photo) VALUES ('$name', '$tutorcode', '$photo')";
+      $addtutor_qry = mysqli_query($dbconnect, $addtutor_sql);
+      echo "$name, $tutorcode";
+      
+      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+    }
+  }
+} else {
+  echo "
+  <div class='alert alert-primary' role='alert'>
+  This tutor already exists!
+  </div>";
+
+}
+
+
+
+
+
+
+
+
+?>
